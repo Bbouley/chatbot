aee0d4c19ac3216d24085cf33525f33b63c7144c
bbouley <b.bouley@hotmail.co.uk>
2016-03-03 10:56:57 -0700

    updated with more options

--- main.js
+++ main.js
@@ -4,17 +4,29 @@ var textIn = document.getElementById('text_in').value;
 // Chat Bot - Bot replies with something based on what is being input - If statements
 
 submitButton.onclick = function () {
-    var textIn = document.getElementById('text_in').value;
+    var textIn = document.getElementById('text_in').value.toLowerCase();
     var message = document.getElementById('user_message');
 
     if (textIn === '') {
         message.innerHTML = 'Theres No Text There!!!'
-    } else if (textIn === 'Bradley') {
+    } else if (textIn === 'bradley') {
         message.innerHTML = 'Nice Name Bro!!'
-    } else if (textIn === 'Zach') {
+    } else if (textIn === 'zach') {
         message.innerHTML = 'Zach is not as cool a name as Bradley. I guess it\'s okay.'
+    } else if (textIn === 'pizza') {
+        message.innerHTML = 'I love PIZZA!!!'
+    } else if (textIn === 'burgers') {
+        message.innerHTML = 'I also love BURGERS!!!'
+    } else if (textIn.toLowerCase() === 'kale') {
+        message.innerHTML = 'Get out...'
+    } else if (textIn === 'hello') {
+        message.innerHTML = '...Is it me you\'re looking for?';
+    } else if (textIn === 'how are you?') {
+        message.innerHTML = 'Awesome. So So Awesome';
+    } else if (textIn.charAt(0) === 'b') {
+        message.innerHTML = 'Sup B-Dawg';
     } else {
-        message.innerHTML = 'Whatever';
+        message.innerHTML = 'Whatever BRO!!!';
     }
 
 }
